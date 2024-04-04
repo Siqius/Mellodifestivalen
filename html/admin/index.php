0dcd07a4fcff4f719a270e503137ba558c2ece4a
Siqius <Real.FBAB@gmail.com>
2024-04-04 15:58:11 +0200

    update

--- html/admin/index.php
+++ html/admin/index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/admin.css">
-    <script src="../js/admin.js" defer></script>
-    <title>admin page</title>
+    <link rel="stylesheet" href="../../css/admin.css">
+    <script src="../../js/admin.js"></script>
+    <title>Admin page</title>
 </head>
 <body>
     
